3023e914c7ea8b3394c054154d7f530a2b5f1a9e
M1M99 <polad197820@gmail.com>
2026-01-08 16:50:37 +0400

    Refactor Sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,17 +1,38 @@
+// import { defineConfig } from "sanity"
+// import { structureTool } from "sanity/structure"
+// import { visionTool } from "@sanity/vision"
+// import { schema } from "./sanity/schema"
+
+// const config = defineConfig({
+//   name: "default",
+//   title: "Baku E-Commerce",
+//   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "test",
+//   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+//   basePath: "/studio",
+//   plugins: [structureTool(), visionTool()],
+//   schema: schema,
+// })
+
+// export { config }
+// export default config
+
+
+
 import { defineConfig } from "sanity"
 import { structureTool } from "sanity/structure"
 import { visionTool } from "@sanity/vision"
-import { schema } from "./sanity/schema"
+import { schema } from "./sanity/schemaTypes" 
+import { structure } from "./sanity/structure"
 
-const config = defineConfig({
+export default defineConfig({
   name: "default",
   title: "Baku E-Commerce",
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "test",
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   basePath: "/studio",
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    structureTool({ structure }),
+    visionTool(),
+  ],
   schema: schema,
 })
-
-export { config }
-export default config
