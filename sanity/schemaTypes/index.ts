c886681a76ddc7975b05625952d6ad91a13c533f
M1M99 <polad197820@gmail.com>
2026-01-08 16:55:19 +0400

    Reorder schema types

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,10 +1,20 @@
+// sanity/schemaTypes/index.ts
 import { type SchemaTypeDefinition } from 'sanity'
 
-import {blockContentType} from './blockContentType'
-import {categoryType} from './categoryType'
-import {postType} from './postType'
-import {authorType} from './authorType'
+import { blockContentType } from './blockContentType'
+import { categoryType } from './categoryType'
+import { authorType } from './authorType'
+
+import { postType } from './postType'
+
+import { product } from './product' 
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, authorType],
+  types: [
+    product,         
+    postType,         
+    authorType,       
+    categoryType,    
+    blockContentType,
+  ],
 }
